c019ddf7daa018dcb17b067b0ceccb9b3bf55a6d
Junya Yamamoto <nitro.mic.ug+github@gmail.com>
2013-03-15 11:09:01 +0900

    Create symlink.sh
    自動シンボリックリンク作成スクリプト

---
+++ symlink.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zshrc.alias ~/.zshrc.alias
